1d7313c543ecf14271ea36f0bc29e34e060e0fc7
Cesar <cesar.jimenez.btw@gmail.com>
2024-09-05 01:21:51 -0400

    Redirect added to login server action

--- actions/login.js
+++ actions/login.js
@@ -1,9 +1,13 @@
 "use server";
 
+import { redirect } from "next/navigation";
+
 export async function login(formData){
     const email = formData.get("email");
     const password = formData.get("password");
 
     console.log("Attempting to login");
     console.log(email, password);
+
+    redirect("/dashboard");
 }
