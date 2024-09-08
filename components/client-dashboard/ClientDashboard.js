4ee68636d448250b217070b19b7b5875a051fa86
cheternal7890 <cesar.jimenez.btw@gmail.com>
2024-09-07 21:15:06 -0400

    Added ability to undo pending entries

--- components/client-dashboard/ClientDashboard.js
+++ components/client-dashboard/ClientDashboard.js
@@ -60,18 +60,19 @@ export default function ClientDashboard() {
 
     const [entries, setEntries] = useState(glucoseList);
 
+    const [pendingEntry, setPendingEntry] = useState([]);
+
     const addNewEntry = () => {
         const newEntry = {
-            id: entries.length + 1,
+            id: Date.now(),
             time: 'Time',
             systolic: '0',
             diastolic: '0',
             pulse: '0',
             notes: 'Notes'
         }
-        setEntries([...entries, newEntry])
-
-        addEntry();
+        setPendingEntry([...pendingEntry, newEntry])
+        console.log(pendingEntry);
     }
 
     const handleEditClick = (id) => {
@@ -82,97 +83,106 @@ export default function ClientDashboard() {
         console.log("Delete item", id)
     }
 
-    return (
-        <div className={styles.container}>
-            <div className={styles.grid}>
-                <div className={styles['left-col']}>
-                    <div className={styles['calendar-container']}>
+    const handleUndo = (id) => {
+        setPendingEntry(pendingEntry.filter((prevState) =>
+            prevState.id !== id
+        ))
+}
 
-                        {isClient && (
-                            <LocalizationProvider dateAdapter={AdapterDayjs}>
-                                <DateCalendar className={styles.calendar} value={selectedDay} onChange={(newValue) => setSelectedDay(newValue)} />
-                            </LocalizationProvider>
-                        )}
+return (
+    <div className={styles.container}>
+        <div className={styles.grid}>
+            <div className={styles['left-col']}>
+                <div className={styles['calendar-container']}>
 
-                    </div>
-                </div>
-
-                <div className={styles['right-col']}>
-                    <div className={styles['date-header']}>
-
-                        <div className={styles['date-flex']}>
-                            <div className={styles['date-styling']}>
-                                <button className={styles['button']} onClick={() => handleDateChange(-1)}>
-                                    <ChevronLeft />
-                                </button>
-
-                                <div className={styles.date}>
-                                    {selectedDay.format('dddd, MMM D')}
-                                </div>
-
-                                <button className={styles['button']} onClick={() => handleDateChange(1)}>
-                                    <ChevronRight />
-                                </button>
-                            </div>
-
-                            <button className={`${styles['date-styling']} ${styles['reset-button']}`} onClick={resetDate}>
-                                Reset
-                            </button>
-                        </div>
-
-                        <div className={styles['date-styling']}>
-                            Cesar Jimenez
-                        </div>
-                    </div>
-
-                    <div className={styles.card}>
-                        <div className={styles['filter-card']}>
-                            <div>Time</div>
-                            <div>Systolic</div>
-                            <div>Diastolic</div>
-                            <div>Pulse</div>
-                            <div>Actions</div>
-                        </div>
-
-                        {/* Data fetched from the server to be displayed*/}
-                        {entries.map((tracker) => {
-                            return (
-                                <div key={tracker.id} className={styles['filter-card']}>
-                                    <div>{tracker.time}</div>
-                                    <div>{tracker.systolic}</div>
-                                    <div>{tracker.diastolic}</div>
-                                    <div>{tracker.pulse}</div>
-                                    <div className={styles.action}>
-                                        <button className={styles.button} onClick={() => handleEditClick(tracker.id)}><Pencil /></button>
-                                        <button className={styles.button} onClick={() => handleDelete(tracker.id)}><Trash2 /></button>
-                                    </div>
-                                </div>
-                            )
-                        })}
-
-
-                        {/* Data that is waiting to be submitted */}
-                        <div className={styles['filter-card']}>
-                            <div contentEditable suppressContentEditableWarning={true} autoFocus>0</div>
-                            <div contentEditable suppressContentEditableWarning={true}>0</div>
-                            <div contentEditable suppressContentEditableWarning={true}>0</div>
-                            <div contentEditable suppressContentEditableWarning={true}>0</div>
-                            <div className={styles.action}>
-                                <button className={styles.button} style={{color: "green"}} onClick={() => handleEditClick(tracker.id)}><CircleCheck /></button>
-                                <button className={styles.button} onClick={() => handleEditClick(tracker.id)}><Undo2 /></button>
-                            </div>
-                        </div>
-
-
-                        <div className={styles['add-container']}>
-                            <button className={styles.add} onClick={addNewEntry}>
-                                Add Tracker
-                            </button>
-                        </div>
-                    </div>
+                    {isClient && (
+                        <LocalizationProvider dateAdapter={AdapterDayjs}>
+                            <DateCalendar className={styles.calendar} value={selectedDay} onChange={(newValue) => setSelectedDay(newValue)} />
+                        </LocalizationProvider>
+                    )}
 
                 </div>
             </div>
+
+            <div className={styles['right-col']}>
+                <div className={styles['date-header']}>
+
+                    <div className={styles['date-flex']}>
+                        <div className={styles['date-styling']}>
+                            <button className={styles['button']} onClick={() => handleDateChange(-1)}>
+                                <ChevronLeft />
+                            </button>
+
+                            <div className={styles.date}>
+                                {selectedDay.format('dddd, MMM D')}
+                            </div>
+
+                            <button className={styles['button']} onClick={() => handleDateChange(1)}>
+                                <ChevronRight />
+                            </button>
+                        </div>
+
+                        <button className={`${styles['date-styling']} ${styles['reset-button']}`} onClick={resetDate}>
+                            Reset
+                        </button>
+                    </div>
+
+                    <div className={styles['date-styling']}>
+                        Cesar Jimenez
+                    </div>
+                </div>
+
+                <div className={styles.card}>
+                    <div className={styles['filter-card']}>
+                        <div>Time</div>
+                        <div>Systolic</div>
+                        <div>Diastolic</div>
+                        <div>Pulse</div>
+                        <div>Actions</div>
+                    </div>
+
+                    {/* Data fetched from the server to be displayed*/}
+                    {entries.map((tracker) => {
+                        return (
+                            <div key={tracker.id} className={styles['filter-card']}>
+                                <div>{tracker.time}</div>
+                                <div>{tracker.systolic}</div>
+                                <div>{tracker.diastolic}</div>
+                                <div>{tracker.pulse}</div>
+                                <div className={styles.action}>
+                                    <button className={styles.button} onClick={() => handleEditClick(tracker.id)}><Pencil /></button>
+                                    <button className={styles.button} onClick={() => handleDelete(tracker.id)}><Trash2 /></button>
+                                </div>
+                            </div>
+                        )
+                    })}
+
+                    {/* Data that is waiting to be submitted */}
+                    {pendingEntry.map((entry) => {
+                        return (
+                            <div key={entry.id} className={styles['filter-card']}>
+                                <div contentEditable suppressContentEditableWarning={true} autoFocus>0</div>
+                                <div contentEditable suppressContentEditableWarning={true}>0</div>
+                                <div contentEditable suppressContentEditableWarning={true}>0</div>
+                                <div contentEditable suppressContentEditableWarning={true}>0</div>
+                                <div className={styles.action}>
+                                    <button className={styles.button} style={{ color: "green" }}><CircleCheck /></button>
+                                    <button className={styles.button} onClick={() => handleUndo(entry.id)}><Undo2 /></button>
+                                </div>
+                            </div>
+                        )
+                    })}
+
+
+                    <div className={styles['add-container']}>
+                        <button className={styles.add} onClick={addNewEntry}>
+                            Add Tracker
+                        </button>
+                    </div>
+                </div>
+
+            </div>
         </div>
-    );
+    </div>
+);
 }
