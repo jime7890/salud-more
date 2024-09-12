a217fc7166c7dcb28943dcabda429752f0d02a78
cheternal7890 <cesar.jimenez.btw@gmail.com>
2024-09-12 03:13:06 -0400

    Adjusting color and transition values

--- components/glucose-dashboard/GlucoseDashboard.js
+++ components/glucose-dashboard/GlucoseDashboard.js
@@ -9,9 +9,29 @@ import PendingEntries from '../pending-entry/PendingEntryForm';
 
 import styles from "@/app/dashboard/page.module.css";
 import shared from "../shared.module.css";
+import { useEffect, useState } from 'react';
 
 export default function GlucoseDashboard() {
+    const [isClient, setIsClient] = useState(false);
+    const [selectedDay, setSelectedDay] = useState(dayjs());
+
+    useEffect(() => {
+        setIsClient(true);
+    }, [])
+
     return (
-        <div>Test</div>
+        <div className={styles.container}>
+            <div className={styles['left-col']}>
+                <Calendar
+                    isClient={isClient}
+                    value={selectedDay}
+                    onChange={(newValue) => setSelectedDay(newValue)}
+                />
+            </div>
+
+            <div className={styles['right-col']}>
+                Test
+            </div>
+        </div>
     )
 }
